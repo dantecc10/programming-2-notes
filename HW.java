149c21264b2718a7a2732710cb8d7712dfa91a15
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-08-15 09:32:00 -0600

    update notes

--- HW.java
+++ HW.java
@@ -1,7 +1,7 @@
-// Your First Program
+// Your First Program in Java
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
     }
 }
