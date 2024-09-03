ffc82b56700782d124bc8b8e07b4fda29d13f694
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-09-03 10:14:55 -0600

    Add Graphic HW

---
+++ HWG.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class HelloGraphic {
+    plublic static void main(string cad[]) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo!", "Mi 2do programa", JOptionPane.INFORMATION_MESSAGE);
+    } 
+}
