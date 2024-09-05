c86a609594a3613d9f58dfcdd05b02cf640ba9c9
Valdo17HR <oswaldohr38@gmail.com>
2024-09-05 10:13:44 -0600

    -

--- practices/Calculadora.java
+++ practices/Calculadora.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculadora {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
         System.out.println("Ingrese el primer número: ");
