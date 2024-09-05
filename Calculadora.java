658985548d2b23046b8976b55cd701e2b83c260f
Valdo17HR <oswaldohr38@gmail.com>
2024-09-05 10:27:25 -0600

    mejorado

--- Calculadora.java
+++ Calculadora.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
 
 public class Calculadora {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String cad[]) {
+        Scanner ent=new Scanner(System.in);
 
-        System.out.println("Ingrese el primer número: ");
-        double num1 = sc.nextDouble();
+        System.out.print("Ingrese el primer número: ");
+        int num1 = ent.nextInt();
 
-        System.out.println("Ingrese el segundo número: ");
-        double num2 = sc.nextDouble();
-
-        double suma = num1 + num2;
-        double resta = num1 - num2;
-        double multiplicacion = num1 * num2;
-        double division = num1 / num2;
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = ent.nextInt();
 
         System.out.println("Resultados: ");
-        System.out.println("Suma: "+suma);
-        System.out.println("Resta: "+resta);
-        System.out.println("Multiplicación: "+multiplicacion);
-        if (num2 !=0){
-            System.out.println("Division: "+division);
-        } else {
-            System.out.println("No se puede dividir entre cero");
-        }
+        System.out.println("Suma: "+(num1+num2));
+        System.out.println("Resta: "+(num1-num2));
+        System.out.println("Multiplicación: "+(num1*num2));
+        
+        float c = (float) num1/num2;
+        System.out.println("Division: "+c);
+        
         sc.close();
 
     }
