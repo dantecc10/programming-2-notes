bf18f19e918e4b223ecb18815588fb79f121ac7e
Valdo17HR <oswaldohr38@gmail.com>
2024-09-05 10:09:20 -0600

    Listo

--- practices/Calculadora.java
+++ practices/Calculadora.java
@@ -24,6 +24,7 @@ public class Operaciones {
         } else {
             System.out.println("No se puede dividir entre cero");
         }
-        sc.close();        
+        sc.close();
+                
     }
 }
