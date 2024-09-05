5ff65572f5010228534887630bf3d47cdf566ebe
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-05 09:41:34 -0600

    update query

--- Padre.java
+++ Padre.java
@@ -3,19 +3,19 @@ public class Padre {
     protected int a, b;
     private float x;
 
-    public Padre () {
+    public Padre() {
         nombre = "";
         a = b = 0;
         x = 0;
     }
 
-    public Padre (String nom) {
+    public Padre(String nom) {
         nombre = nom;
         a = b = 0;
         x = 0;
     }
 
-    public Padre (String nombre, int a, int b, float x) {
+    public Padre(String nombre, int a, int b, float x) {
         this.nombre = nombre;
         this.a = a;
         this.b = b;
