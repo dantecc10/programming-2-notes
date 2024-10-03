d78e203b6fab3ea0f2898b2200cf9ecb370e7491
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-10-03 11:09:52 -0600

    updating code

--- practices/instruments/Guitar.java
+++ practices/instruments/Guitar.java
@@ -1,9 +1,13 @@
 package practices.instruments;
 
 public class Guitar extends Instrument {
+    public String name;
+    public String sound;
 
     public Guitar(String name, String sound) {
         super(name, sound);
+        this.name = name;
+        this.sound = sound;
     }
 
     public void play() {
