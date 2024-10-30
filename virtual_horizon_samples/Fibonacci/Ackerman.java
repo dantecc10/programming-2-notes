7f5cb72eac9fe485eb15df26f69b3a000c6cb15d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-10-30 10:24:16 -0600

    sh

--- virtual_horizon_samples/Fibonacci/Ackerman.java
+++ virtual_horizon_samples/Fibonacci/Ackerman.java
@@ -1,3 +1,11 @@
 package virtual_horizon_samples.Fibonacci;
 
-public static int acm(int m,int n){if(m==0){return n+1;}else if(n==0){return acm(m-1,1);}else{return acm(m-1,acm(m,n-1));}}
+public static int acm(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return acm(m - 1, 1);
+    } else {
+        return acm(m - 1, acm(m, n - 1));
+    }    
+}
