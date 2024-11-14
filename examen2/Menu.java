88104b9f009f9433e1184ed6637b35b214e59e0b
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-11-14 13:03:18 -0600

    correcta implementacion - Menu modificado - Temperatura conclusa

--- examen2/Menu.java
+++ examen2/Menu.java
@@ -8,60 +8,64 @@ public class Menu {
     public static void main(String[] args) throws OutBoundsException {
 
         Scanner input = new Scanner(System.in);
-
-        System.out.print("""
-            --------------| Examen #2 |--------------
-                    Por favor eliga una opcion
-
-            1.               Conversor de temperatura
-            2.     Descripcion de animales (Herencia)
-            3.      Sonidos de animales (Abstraccion)
-            4.                      Manejo de activos
-            5.                                  Salir
-            """);
-
         int opc = 0;
-        boolean flag = false;
-        
-        while(!flag) {
-            try {
 
-                System.out.print("-----------------------------------------\nOpcion: ");
-                opc = input.nextInt();
-                if (opc < 1 || opc > 5) {
-                    throw new OutBoundsException();
-                }
-                flag = true;
-                
-            } catch (InputMismatchException e) {
-                System.err.print("Opcion no numerica");
-            } catch (OutBoundsException e) {
-                System.err.print(e.getMessage());
-            } finally {
-                input.nextLine();
-                if (!flag) {
-                    System.out.println(", intente de nuevo");
+        while (opc != 5) {
+            clean();
+            System.out.print("""
+                --------------| Examen #2 |--------------
+                        Por favor elija una opcion
+
+                1.               Conversor de temperatura
+                2.     Descripcion de animales (Herencia)
+                3.      Sonidos de animales (Abstraccion)
+                4.                      Manejo de activos
+                5.                                  Salir
+                """);
+
+            boolean flag = false;
+            
+            while(!flag) {
+                try {
+
+                    System.out.print("-----------------------------------------\nOpcion: ");
+                    opc = input.nextInt();
+                    if (opc < 1 || opc > 5) {
+                        throw new OutBoundsException();
+                    }
+                    flag = true;
+                    
+                } catch (InputMismatchException e) {
+                    System.err.print("Opcion no numerica");
+                } catch (OutBoundsException e) {
+                    System.err.print(e.getMessage());
+                } finally {
+                    input.nextLine();
+                    if (!flag) {
+                        System.out.println(", intente de nuevo");
+                    }
                 }
             }
-        }
 
-        switch (opc) {
-            case 1:
-                new Temperatura();
-                break;
-            case 2:
-                // Herencia
-                break;
-            case 3:
-                // Abstraccion
-                break;
-            case 4:
-                // Monedero
-                break;
-            case 5:
-                Menu.clean();
-                System.out.print("\nSaliendo...");
-                break;
+            switch (opc) {
+                case 1:
+                    clean();
+                    new Temperatura();
+                    break;
+                case 2:
+                    // Herencia
+                    break;
+                case 3:
+                    // Abstraccion
+                    break;
+                case 4:
+                    // Monedero
+                    break;
+                case 5:
+                    clean();
+                    System.out.print("\nSaliendo...");
+                    break;
+            }
         }
     }
 
--- examen2/Temperatura.java
+++ examen2/Temperatura.java
@@ -1,23 +1,98 @@
 package examen2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Temperatura {
     
     // Crea un menu para la opcion
     public Temperatura() {
+
+        Scanner input = new Scanner(System.in);
 
         System.out.println("""
             -------------| Conversor de temperatura |-------------
             Bienvenido, ingresa un valor con el siguiente formato:
                                   36 C, 36 F                      
 
-            ------------------------------------------------------
-        """);
+                    "Para salir digite la temperatura 999"        
+            """);
+        
+        double tempValue = 0;
+        char scale = ' ';
+        boolean flag = false;
+        boolean exit = false;
+
+        while (!exit) {
+            while(!flag) {
+                System.out.print("------------------------------------------------------\nTemperatura: ");
+            
+                String temp = input.nextLine();
+                String[] data = temp.split(" ");
+
+                if(isValid(data)) {
+                    tempValue = Double.parseDouble(data[0]);
+                    scale = data[1].charAt(0);
+                    flag = true;
+                } else {
+                    System.out.println("Temperatura invalida, intente de nuevo");
+                }
+            }
+
+            if (tempValue != 999.0) {
+                Double convertedValue = convert(tempValue, scale);
+                String message = "es igual a";
+                String[] scaleMessage = {"C", "F"};
+                int m1, m2;
+                if (scale == 'c' || scale == 'C') {
+                    m1 = 0;
+                    m2 = 1;
+                } else {
+                    m1 = 1;
+                    m2 = 0;
+                }
+                String output = String.format("%.1f %s %s %.1f %s ", tempValue, scaleMessage[m1], message, convertedValue, scaleMessage[m2]);
+                System.out.println(output + "\n------------------------------------------------------\n");
+
+                flag = false;
+            } else {
+                exit = true;
+            }
+        }
 
     }
 
-    // En proceso
-    public void Lectura() {
+    // Comprueba si la entrada corresponde al formato solicitado
+    public boolean isValid(String[] data) {
+        if (data.length != 2) {
+            return false;
+        } else {
+            try {
+                Double.parseDouble(data[0]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
 
+            if (data[1].equalsIgnoreCase("c") || data[1].equalsIgnoreCase("f")) {
+                return true;
+            }
+            return false;
+        }
     }
 
+    public Double convert(double temp, char scale) {
+
+        // Dependiendo la escala ingresada se convierte de una forma, u otra
+        if (scale == 'c' || scale == 'C') {
+            temp *= 9.0;
+            temp += 160.0;
+            temp /= 5.0;
+        } else {
+            temp -= 32.0;
+            temp *= 5.0;
+            temp /= 9.0;
+        }
+
+        return temp;
+    }
 }
